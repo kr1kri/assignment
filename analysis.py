d078771a8fa1c0819f6088dfed24be241ce84a2f
Christos Christou <kr1kri@protonmail.com>
2019-07-06 22:11:31 +0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -13,8 +13,14 @@ comments = []
 for item in data:
     comments.append(item["text"])
 
+with open('gr_stopwords.txt') as f:
+    for line in f:
+        line = line.strip()
+        mystopwords.append(line)
 
-wordcloud = WordCloud(max_font_size=40, relative_scaling=0.5, background_color="white").generate(" ".join(comments))
+
+wordcloud = WordCloud(max_font_size=40, relative_scaling=0.5, stopwords=mystopwords,
+                      background_color="white").generate(" ".join(comments))
 
 plt.figure()
 plt.imshow(wordcloud, interpolation='bilinear')
