a615ac6fad46c8d5097d0bd21df56a67f4cb04b8
Christos Christou <kr1kri@protonmail.com>
2019-07-08 16:22:42 +0300

    Update analysis.py

--- analysis.py
+++ analysis.py
@@ -1,24 +1,25 @@
+#import modules
 import json
-import os
 import matplotlib.pyplot as plt
-from wordcloud import WordCloud, STOPWORDS
-import numpy
+from wordcloud import WordCloud
+import pandas as pd
 
 #read the dataset
-with open('file.json') as f:
+with open('file.json', 'r') as f:
     data = json.load(f)
 
+#categories to execute wordcloud
 categories = ["text", "username"]
 
-#create stopwords for wordcloud
+#add stopwords for wordcloud
 mystopwords = []
 
 #pass the text of gr_stopwords as stopwords for wordcloud
 with open('gr_stopwords.txt') as f:
     mystopwords = f.read().splitlines()
 
-#create wordclouds
-for x in range(len(categories)):
+#wordclouds creation
+for x in range (len(categories)):
     column = []
 
     for item in data:
@@ -31,6 +32,24 @@ for x in range(len(categories)):
     plt.figure(figsize=(20, 10))
     plt.imshow(wordcloud, interpolation="bilinear")
     plt.axis("off")
-    plt.savefig("wordcloud" + categories[x] + ".png")
+    plt.savefig("wordcloud_" + categories[x] + ".png")
+
+#initialize a dataframe with the publish timespamp
+df = pd.DataFrame([[pd.to_datetime(x['timestamp']),i] for i,x in enumerate(data)], columns=['timespamp', 'id'])
+
+#group by year and month in order to visualize the distribution of the publish date
+ax = df.groupby([df["timespamp"].dt.year, df["timespamp"].dt.month, df["timespamp"].dt.day])['id'].count().plot(title='Publish Date Distribution')
+ax.set_ylabel("number of posts")
+ax.set_xlabel("date")
+
+plt.savefig("publish_date.png")
+
+#group by hour in order to visualize the distribution of the publish time
+ax = df.groupby([df["timespamp"].dt.hour])['id'].count().plot(title='Publish Time Distribution')
+ax.set_ylabel("number of posts")
+ax.set_xlabel("hour of day")
+
+plt.savefig("publish_time.png")
+
 
 
